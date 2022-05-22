9e4971eb2bc7829b812e3d45edd93f0a8b8f5949
xwc2022 <xiangwei71@gmail.com>
2022-05-22 20:02:27 +0800

    動態改材質

--- WebModelViewer/test.js
+++ WebModelViewer/test.js
@@ -5,15 +5,14 @@ const createScene = function () {
 
     const scene = new BABYLON.Scene(engine);
 
-    const camera = new BABYLON.ArcRotateCamera("camera", 0, Math.PI / 2.5, 5, new BABYLON.Vector3(0, 0, 0));
+    const camera = new BABYLON.ArcRotateCamera("camera", Math.PI * 0.5, Math.PI * 0.5, 5, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
 
     return scene;
 };
 
-
-const scene = createScene(); //Call the createScene function
+const scene = createScene();
 
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () {
@@ -25,6 +24,21 @@ window.addEventListener("resize", function () {
     engine.resize();
 });
 
-BABYLON.SceneLoader.Append("GLTF/", "E_act_move.glb", scene, function (scene) {
-    scene.activeCamera.alpha += 0.5 * Math.PI;
-});
+// BABYLON.SceneLoader.Append("GLTF/", "E_act_move.glb", scene, function (scene) {
+//     scene.activeCamera.alpha += 0.5 * Math.PI;
+// });
+
+const importPromise = BABYLON.SceneLoader.ImportMeshAsync(["MotoBody"], "GLTF/", "bike_3.gltf", scene);
+// const importPromise = BABYLON.SceneLoader.ImportMeshAsync(["Armature"], "GLTF/", "E_act_move.gltf", scene);
+importPromise.then((result) => {
+    // Result has meshes, particleSystems, skeletons, animationGroups and transformNodes
+    console.log(result);
+
+    // 移動
+    result.transformNodes[0].position = new BABYLON.Vector3(0, -0.5, 0);
+
+    // 動態改材質
+    var m = new BABYLON.StandardMaterial("myMaterial", scene);
+    m.diffuseColor = new BABYLON.Color3(0, 1, 0);
+    result.meshes[3].material = m;
+})
