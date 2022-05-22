19cb47086a1ffe5c9c61caeed2b8ba26372773e8
xwc2022 <xiangwei71@gmail.com>
2022-05-22 15:41:40 +0800

    testBabylonLoadGLTF

---
+++ WebModelViewer/test.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas, true);
+
+const createScene = function () {
+
+    const scene = new BABYLON.Scene(engine);
+
+    const camera = new BABYLON.ArcRotateCamera("camera", 0, Math.PI / 2.5, 5, new BABYLON.Vector3(0, 0, 0));
+    camera.attachControl(canvas, true);
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+    return scene;
+};
+
+
+const scene = createScene(); //Call the createScene function
+
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+    scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+    engine.resize();
+});
+
+BABYLON.SceneLoader.Append("GLTF/", "bike_3.glb", scene, function (scene) {
+    scene.activeCamera.alpha += 0.5 * Math.PI;
+});
